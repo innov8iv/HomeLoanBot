049d9b4796d71ccecdfddda6c3ab0aad63832dfd
Gerard Dillon <gerard.dillon@innov8iv.com>
2018-03-13 21:42:45 +1100

    Push files to Github

---
+++ app.js
@@ -0,0 +1,81 @@
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
+
+var builder = require('botbuilder');
+var restify = require('restify');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot and listen to messages
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+server.post('/api/messages', connector.listen());
+
+var DialogLabels = {
+    Loan: 'How Much Can I Loan?',
+    Repay: 'How Much Can I Repay?',
+    Support: 'Support'
+};
+
+var bot = new builder.UniversalBot(connector, [
+    // What is your name
+    function (session) {
+        builder.Prompts.text(session, "Welcome to the BrokerChat\n\rWhat's your Name?");
+    },
+    function (session, results, next) {
+        session.dialogData.FirstName = results.response;
+        next();
+    },        
+    function (session) {        
+        // prompt for search option
+        builder.Prompts.choice(
+            session,
+            'Hi ' + session.dialogData.FirstName + ', let me help you work out what you can afford?',
+            [DialogLabels.Loan, DialogLabels.Repay],
+            {
+                maxRetries: 3,
+                retryPrompt: 'Not a valid option',
+                listStyle: builder.ListStyle.button
+            });
+    },
+    function (session, result) {
+        if (!result.response) {
+            // exhausted attemps and no selection, start over
+            session.send('Ooops! Too many attempts :( But don\'t worry, I\'m handling that exception and you can try again!');
+            return session.endDialog();
+        }
+
+        // on error, start over
+        session.on('error', function (err) {
+            session.send('Failed with message: %s', err.message);
+            session.endDialog();
+        });
+
+        // continue on proper dialog
+        var selection = result.response.entity;
+        switch (selection) {
+            case DialogLabels.Loan:
+                return session.beginDialog('How Much Can I Loan?');
+            case DialogLabels.Repay:
+                return session.beginDialog('How Much Can I Repay?');
+        }
+    }
+]);
+
+bot.dialog('How Much Can I Loan?', require('./loan'));
+bot.dialog('How Much Can I Repay?', require('./repayments'));
+bot.dialog('support', require('./support'))
+    .triggerAction({
+        matches: [/help/i, /support/i, /problem/i]
+    });
+
+// log any bot errors into the console
+bot.on('error', function (e) {
+    console.log('And error ocurred', e);
+});
---
+++ repayments.js
@@ -0,0 +1,98 @@
+var builder = require('botbuilder');
+var Store = require('./store');
+var LoanCalc = require('loan-calc');
+
+module.exports = [
+
+    // Value 
+    function (session) {
+        builder.Prompts.number(session, "How much do you want to borrow?");
+    },
+    function (session, results, next) {
+        session.dialogData.BorrowingAmount = results.response;
+        next();
+    },
+
+    // Check-in
+    function (session) {
+        builder.Prompts.number(session, "How many years do you want to pay it back over?");
+    },
+    function (session, results, next) {
+        session.dialogData.LoanYears = results.response;
+        next();
+    },
+
+    // Nights
+    function (session) {
+        builder.Prompts.number(session, "What is the maximum monthly repayment you can afford?");
+    },
+    function (session, results, next) {
+        session.dialogData.AcceptableMonthlyRepayment = results.response;
+        next();
+    },
+
+    function (session) {
+        //Calculate monthly repayments on loan amount based on interest rate of 6%
+        session.dialogData.ActualMonthlyRepayment = LoanCalc.paymentCalc({
+            amount: session.dialogData.BorrowingAmount,
+            rate: 6,
+            termMonths: session.dialogData.LoanYears*12
+        });
+
+        if (session.dialogData.ActualMonthlyRepayment <= session.dialogData.AcceptableMonthlyRepayment)
+            session.send("Congratulations "+session.dialogData.FirstName+"!  The actual monthly repayment would be: $"+session.dialogData.ActualMonthlyRepayment);
+        else
+            session.send("Unfortunately "+session.dialogData.FirstName+", the actual monthly repayment would be: $"+session.dialogData.ActualMonthlyRepayment);
+        session.endDialog();        
+    }
+
+    // Search...
+    // function (session) {
+    //     var destination = session.dialogData.destination;
+    //     var checkIn = new Date(session.dialogData.checkIn);
+    //     var checkOut = checkIn.addDays(session.dialogData.nights);
+
+    //     session.send(
+    //         'Ok. Searching for Hotels in %s from %d/%d to %d/%d...',
+    //         destination,
+    //         checkIn.getMonth() + 1, checkIn.getDate(),
+    //         checkOut.getMonth() + 1, checkOut.getDate());
+
+    //     // Async search
+    //     Store
+    //         .searchHotels(destination, checkIn, checkOut)
+    //         .then(function (hotels) {
+    //             // Results
+    //             session.send('I found in total %d hotels for your dates:', hotels.length);
+
+    //             var message = new builder.Message()
+    //                 .attachmentLayout(builder.AttachmentLayout.carousel)
+    //                 .attachments(hotels.map(hotelAsAttachment));
+
+    //             session.send(message);
+
+    //             // End
+    //             session.endDialog();
+    //         });
+    // }
+];
+
+// // Helpers
+// function hotelAsAttachment(hotel) {
+//     return new builder.HeroCard()
+//         .title(hotel.name)
+//         .subtitle('%d stars. %d reviews. From $%d per night.', hotel.rating, hotel.numberOfReviews, hotel.priceStarting)
+//         .images([new builder.CardImage().url(hotel.image)])
+//         .buttons([
+//             new builder.CardAction()
+//                 .title('More details')
+//                 .type('openUrl')
+//                 .value('https://www.bing.com/search?q=hotels+in+' + encodeURIComponent(hotel.location))
+//         ]);
+// }
+
+// Date.prototype.addDays = function (days) {
+//     var date = new Date(this.valueOf());
+//     date.setDate(date.getDate() + days);
+//     return date;
+// };
